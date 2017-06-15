2777c3c44aa581779e5be4c74ae152583f1b24c5
kitdemonio <ddd48533@gmail.com>
2017-06-15 22:50:24 +0200

    Add search functionality

--- src/app/components/Search/Search.jsx
+++ src/app/components/Search/Search.jsx
@@ -7,7 +7,7 @@ export default class Search extends Component {
     render() {
         return (
             <div className="Search bottom-border">
-                <input type="text" placeholder="Search" className="input-default"/>
+                <input type="text" placeholder="Search" className="input-default" onChange={this.props.handleChange}/>
             </div>
         )
     }
--- src/app/containers/FriendList.jsx
+++ src/app/containers/FriendList.jsx
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import SearchInput, {createFilter} from 'react-search-input';
 
 import '../../styles/containers/FriendList.css';
 import Search from '../components/Search/Search';
 import Friend from '../components/Friend/Friend';
 
-export default class FriendList extends Component {
+const KEYS_TO_FILTERS = ['firstName', 'lastName', 'email'];
 
+export default class FriendList extends Component {
+    constructor() {
+        super()
+        this.getUsers();
+
+        this.state = {
+            usersList: [],
+            searchTerm: ''
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+
+    getUsers() {
+        let self = this;
+        firebase.database().ref('/users').once('value').then(function(snapshot) {
+            self.setState({
+                usersList: snapshot.val()
+            })
+        });
+    }
 
     render() {
+        const searchResult = Object.keys(this.state.usersList).filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
         return (
             <div className="container Friend-List">
-                <Search />
-                <ul>
-                    <li>
-                        <Friend />
-                        <Friend />
-                        <Friend />
-                        <Friend />
-                        <Friend />
-                        <Friend />
-                        <Friend />
+                <Search handleChange={this.handleChange.bind(this)}/>
+                <ul>                
+                {searchResult.map((value,i) => {
+                    return (
+                    <li key={i}>
+                        <Friend email={this.state.usersList[value].email}/>
                     </li>
+                    )
+                })}
                 </ul>
             </div>
         )
