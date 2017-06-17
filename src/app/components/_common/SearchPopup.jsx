14b70d9d23bf410f2f68a65d8b82fd52881bec70
kitdemonio <ddd48533@gmail.com>
2017-06-17 16:36:18 +0200

    Feature: friend search

---
+++ src/app/components/_common/SearchPopup.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import SearchInput, {createFilter} from 'react-search-input';
+import * as firebase from 'firebase';
+
+const popupStyles = {
+	border: '1px solid #eee',
+	position: 'absolute',
+	zIndex: '10',
+	width: '100%',
+	background: '#fff',
+	top: '25px',
+	left: '230px',
+	padding: '10px 5px',
+	borderRadius: '5px'
+}
+
+const inputStyles = {
+	border: '1px solid #eee',
+	padding: '5px 10px',
+	width: '100%',
+}
+
+const resultStyles = {
+	marginTop: '10px'
+}
+
+const KEYS_TO_FILTERS = ['firstName', 'lastName', 'email'];
+
+class SearchPopup extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			searchTerm: '',
+			usersList: [],
+		}
+	}
+
+	componentDidMount() {
+		this.searchUsers();
+	}
+
+	searchUsers() {
+		let self = this;
+		firebase.database().ref('/users').once('value').then(function (snapshot) {
+			const object = snapshot.val();
+			self.setState({
+				usersList: Object.keys(object).map((key) => {
+					return object[key]
+				})
+			})
+		});
+    }
+
+	handleChange(e) {
+        this.setState({
+            searchTerm: e.target.value
+        })
+    }
+	
+	render() {
+		const searchResult = this.state.usersList.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
+
+		return (
+			<div className="Search-Popup" style={popupStyles}>
+				<input type="text" className="input-default" placeholder="Enter friend name or email" 
+				style={inputStyles} 
+				onChange={this.handleChange.bind(this)}/>
+
+				<ul className="search-result" style={resultStyles}>
+					{ this.state.searchTerm.length === 0 ? '' : searchResult.map((user,i) => {
+						return (
+							<li key={i}>{user.email}</li>
+						)
+					})}
+				</ul>
+
+			</div>
+		);
+	}
+}
+
+export default SearchPopup;
--- src/app/containers/Inbox.jsx
+++ src/app/containers/Inbox.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
-
+import SearchPopup from '../components/_common/SearchPopup';
 import Navigation from '../components/Navigation/Navigation';
 import '../../styles/containers/Inbox.css';
 
 export default class Inbox extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            showSearchPopup: false
+        }
+    }
+
+    showSearchPopup() {
+        this.setState({
+            showSearchPopup: !this.state.showSearchPopup
+        })
+    }
 
     render() {
         return (
             <div className="container Inbox">
                 <h2>Inbox</h2>
+                <span style={{ float: 'right', cursor: 'pointer',position: 'relative' }} 
+                onClick={this.showSearchPopup.bind(this)}>Iconka zamist teksta</span>
+                { this.state.showSearchPopup ? <SearchPopup/> : ''}
                 <Navigation/>
             </div>
         )
