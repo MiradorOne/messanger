b27710616f63ef05daf884eb77beca0fa0f4f9d9
kitdemonio <ddd48533@gmail.com>
2017-06-24 23:16:21 +0300

    Remastered search pop up menu (reworked hovers and added adaptive positioning)

--- src/app/containers/FriendList.jsx
+++ src/app/containers/FriendList.jsx
@@ -32,7 +32,7 @@ export default class FriendList extends Component {
     getConversation(uid) {
         let self = this;
         firebase.database().ref(`/users/${uid}/conversations`).on('value', function(snapshot) {
-                
+
             const result = snapshot.val() || null;
 
             if (snapshot.val()) {
@@ -49,9 +49,9 @@ export default class FriendList extends Component {
 
                         result = {
                             [value]: conversationDetails[0],
-                        }   
+                        }
                     }
-                    
+
                 }).then(() => {
                     if (result && result !== "null") {
                         self.setState({
@@ -67,18 +67,20 @@ export default class FriendList extends Component {
         })
     }
 
-    render() {       
+    render() {
         const searchResult = Object.keys(this.state.conversations)
                             .map(value => {return {
                                 ...this.state.conversations[value],
                                 convID: value
                             }})
                             .filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS));
-                            
+
         const noConversationsMessage = () => {
             return (
-                <li className="no-conversation">
+                <li>
+                 <div className="no-conversation">
                     You don't have conversations yet! Search friend to start conversation
+                 </div>
                 </li>
             )
         }
@@ -96,14 +98,14 @@ export default class FriendList extends Component {
                         Object.keys(this.state.conversations).length === 0 ? noConversationsMessage() :
                         searchResult.map((value, i) => {
                             return (
-                                <li key={i} 
-                                className={this.state.conversations[this.props.activeConversation] && 
-                                    this.state.conversations[this.props.activeConversation].email === value.email ? 'selected' : ''} 
+                                <li key={i}
+                                className={this.state.conversations[this.props.activeConversation] &&
+                                    this.state.conversations[this.props.activeConversation].email === value.email ? 'selected' : ''}
 
                                 onClick={this.props.selectConversation.bind(this,Object.keys(this.state.conversations)[i])}>
-                                    <Friend firstName={value.firstName} 
+                                    <Friend firstName={value.firstName}
                                     lastName={value.lastName}
-                                    convID={value.id} 
+                                    convID={value.id}
                                     />
                                 </li>
                             )
