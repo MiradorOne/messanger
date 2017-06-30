2cff9d6ce7e0f6d41bda8620a986eecb1f8f3110
kitdemonio <ddd48533@gmail.com>
2017-06-30 16:33:55 +0300

    add user : User-List styles reworked

--- src/app/components/_common/NewFriend.jsx
+++ src/app/components/_common/NewFriend.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 const NewFriend = ({firstName, id, lastName, startConversation, email}) => {
 	return (
 		<div className="New-Friend">
+		<div className="position-align">
+		  <div className="user-list-image"></div>
 			<div className="fisrtName">{firstName}</div>
 			<div className="SecondName">{lastName}</div>
-			<p><div className="email">{email}</div></p>
-			<hr></hr>
+			<div className="email">{email}</div>
+		</div>
 			<div className="StartConversation" onClick={startConversation.bind(this,{
 				firstName,
 				lastName,
 				email,
 				id
-			})}> Start <span>&#8594;</span></div>
+			})}>
+			<div className="user-list-button-position">
+		      <div className="user-list-button">Add</div>
+		    </div>
+			</div>
 
 </div>
 	);
