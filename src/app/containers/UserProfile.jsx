b13c53efbc11fde1759f7499180dc0a2c325df53
Dmytro Fedko <dfedko@alliedcoders.com>
2017-08-01 17:33:34 +0200

    Add profile data

--- src/app/containers/UserProfile.jsx
+++ src/app/containers/UserProfile.jsx
@@ -1,39 +1,64 @@
 import React, {Component} from 'react';
 import '../../styles/containers/UserProfile.css';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import {browserHistory, Link} from 'react-router';
 import Placeholder from '../../static/images/friend-image.png';
+import {firebaseConnect, pathToJS} from 'react-redux-firebase';
+import {connect} from 'react-redux';
 
 
-export default class UserProfile extends Component {
-    constructor() {
-        super();
+class UserProfile extends Component {
+    constructor(props) {
+        super(props);
 
         this.state = {
             isEditable: false,
         }
+
+        this.updateProfile = this.updateProfile.bind(this);
     }
+
+    handleChange(field, e) {
+        this.setState({
+            [field]: e.target.value
+        })
+    }
+
+    updateProfile() {
+        this.props.firebase.update(`users/${this.props.auth.uid}/`, {
+            nickname: this.state.nickname,
+            telephone: this.state.telephone,
+            date: this.state.date,
+            gender: this.state.gender,
+            language: this.state.language,
+        }).then(() => {
+            this.setState({
+                isEditable: false
+            })
+        })
+    }
+
     render() {
 
-        const fields = () => {
+        const fields = ({...profile}) => {
             if (this.state.isEditable) {
                 return (
                     <ul>
-                        <li>Nickname: <span><input className="input-default" type="text" placeholder="Lorem"/></span></li>
-                        <li>Telephone: <span><input className="input-default" type="text" placeholder="987 654 321"/></span></li>
-                        <li>Date of Birth: <span><input className="input-default" type="date" placeholder="17.02.1990"/></span></li>
-                        <li>Gender: <span><input className="input-default" type="text" placeholder="Male"/></span></li>
-                        <li>Language: <span><input className="input-default" type="text" placeholder="English"/></span></li>
+                         <li>Nickname: <span><input onChange={this.handleChange.bind(this,'nickname')} className="input-default" type="text" placeholder={profile.nickname}/></span></li>
+                        <li>Telephone: <span><input onChange={this.handleChange.bind(this,'telephone')} className="input-default" type="text" placeholder={profile.telephone}/></span></li>
+                        <li>Date of Birth: <span><input onChange={this.handleChange.bind(this,'date')} className="input-default" type="date" placeholder={profile.date}/></span></li>
+                        <li>Gender: <span><input onChange={this.handleChange.bind(this,'gender')} className="input-default" type="text" placeholder={profile.gender}/></span></li>
+                        <li>Language: <span><input onChange={this.handleChange.bind(this,'language')} className="input-default" type="text" placeholder={profile.language}/></span></li> 
                     </ul>
                 )
             } else {
                 return (
                     <ul>
-                        <li>Nickname: <span>Lorem</span></li>
-                        <li>Telephone: <span>987 654 321</span></li>
-                        <li>Date of Birth: <span>17.02.1990</span></li>
-                        <li>Gender: <span>Male</span></li>
-                        <li>Language: <span>English</span></li>
+                        <li>Nickname: <span>{profile.nickname}</span></li>
+                        <li>Telephone: <span>{profile.telephone}</span></li>
+                        <li>Date of Birth: <span>{profile.date}</span></li>
+                        <li>Gender: <span>{profile.gender}</span></li>
+                        <li>Language: <span>{profile.language}</span></li>
                     </ul>
                 )
             }
@@ -52,15 +77,18 @@ export default class UserProfile extends Component {
                         <div className="profile-info">
                             <div className="full-name">
                                 <p>
-                                    First Last
+                                    {this.props.profile && this.props.profile.firstName + ' ' + this.props.profile.lastName}
                                     <i className={`icon ${this.state.isEditable ? 'icon-edit-active' : 'icon-edit'}`}
                                        onClick={() => {this.setState({isEditable: !this.state.isEditable})}}></i>
                                 </p>
                             </div>
                             <div className="info-list edit">
-                                {fields()}
+                                 {fields(this.props && this.props.profile)} 
                             </div>
-                        </div>
+                        </div>                        
+                    </div>
+                    <div className="row">
+                        {this.state.isEditable ? <button style={{color: '#000'}} onClick={this.updateProfile} className='btn btn-default'>Save</button> : ''}
                     </div>
                 </main>
                 <footer>
@@ -70,3 +98,12 @@ export default class UserProfile extends Component {
         )
     }
 }
+
+const wrapper = firebaseConnect()(UserProfile);
+
+export default connect(
+    ({ firebase }) => ({
+        auth: pathToJS(firebase, 'auth'),
+        profile: pathToJS(firebase, 'profile')
+    })
+)(wrapper);
